e79549899c7acd8456b3cb427bdb6900fe4362e7
Manuel Tiago Pereira <mt.pereira@gmail.com>
2019-10-04 15:24:56 +0100

    Add a command-line interface
    In order to use the Screenshooter, a CLI was added. It allows for
    pointing to a URL and taking a screenshot of it.

---
+++ cmd/hugo-post-preview/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"time"
+
+	s "github.com/mtpereira/hugo-post-preview"
+)
+
+func main() {
+	post := flag.String("post", "", "Name of the post")
+	filename := flag.String("filename", "", "Filename for the screenshot. Defaults to the value of -post.")
+	element := flag.String("element", "article.post", "Name of the CSS ID to select and capture.")
+	siteURL := flag.String("url", "http://localhost:1313", "URL for the website. Defaults to the hugo server default address.")
+	postsPath := flag.String("postsPath", "post", "Path for the posts.")
+	timeout := flag.Duration("timeout", time.Duration(time.Second*10), "Time to wait before giving up on the request to the site. Disabled by setting a negative value.")
+	savePath := flag.String("savePath", ".", "Path for saving the taken screenshots.")
+	flag.Parse()
+
+	timeoutConfig := func(ss *s.Screenshooter) {
+		ss.Timeout = *timeout
+	}
+
+	savePathConfig := func(ss *s.Screenshooter) {
+		ss.SavePath = *savePath
+	}
+
+	if *post == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if *filename == "" {
+		*filename = *post
+	}
+
+	postURL, err := url.Parse(fmt.Sprintf("%s/%s/%s", *siteURL, *postsPath, *post))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	ss := s.New(timeoutConfig, savePathConfig)
+
+	err = ss.Take(*postURL, *element, *filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
